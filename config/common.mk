1956c021498e345dff934e7496c2d87d7a66bcdc
Arman Altafi <84268732+Arman-ATI@users.noreply.github.com>
2025-04-28 19:06:15 +0330

    configs: Include sign keys

--- config/common.mk
+++ config/common.mk
@@ -420,4 +420,6 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
     
 include vendor/lineage/config/version.mk
 
+-include vendor/lineage-priv/keys/keys.mk
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
